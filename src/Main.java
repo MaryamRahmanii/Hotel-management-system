a588998f519bd4ca71f25449b951c75e81ea2004
MaryamRahmani <maryam.rahmani458982@gmail.com>
2024-01-17 22:41:40 +0330

    first page and second page with swing

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -5,29 +6,37 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        final String url="jdbc:mysql://localhost:3306/hotelsystem";
-        final String user="root";
+//        final String url="jdbc:mysql://localhost:3306/hotelsystem";
+//        final String user="root";
+//
+//        Connection com;
+//
+//        {
+//            try {
+//                com = DriverManager.getConnection(url,user,null);
+//            } catch (SQLException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//
+//        String add="INSERT INTO guests(id,email,kodmeli,lastname,name,password)"+"VALUES(?,?,?,?,?,?)";
+//
+//        PreparedStatement pst=com.prepareStatement(add);
+//
+////        pst.setInt(1,3);
+////        pst.setString(2,"maryam@gmail.com");
+////        pst.setString(3,"2081177020");
+////        pst.setString(4,"cherati");
+////        pst.setString(5,"nima");
+////        pst.setString(6,"78910");
+////        pst.executeUpdate();
+//
+//        setTitle("Registration Form");
+//        setSize(400, 300);
+//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        Connection com;
 
-        {
-            try {
-                com = DriverManager.getConnection(url,user,null);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
+    new Hotel();
 
-        String add="INSERT INTO guests(id,email,kodmeli,lastname,name,password)"+"VALUES(?,?,?,?,?,?)";
-
-        PreparedStatement pst=com.prepareStatement(add);
-
-        pst.setInt(1,2);
-        pst.setString(2,"maryam@gmail.com");
-        pst.setString(3,"2081177020");
-        pst.setString(4,"cherati");
-        pst.setString(5,"nima");
-        pst.setString(6,"78910");
-        pst.executeUpdate();
     }
 }
